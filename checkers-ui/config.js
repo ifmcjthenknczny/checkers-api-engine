c24075dfb7a260530f3be290fcb2b3e56a62472d
ifmcjthenknczny <maciej.konieczny.1993@gmail.com>
2026-03-07 17:23:27 +0100

    remove js

--- checkers-ui/config.js
+++
@@ -1,9 +0,0 @@
-import { range } from './helpers.js'
-
-const MAX_GAMES_TO_PLAY = 40_000;
-const BOARD_SIZE = 8;
-const cols = range(BOARD_SIZE, "a");
-const rows = range(BOARD_SIZE, 1);
-const MOVE_ANIMATION_DURATION_MS = 1;
-
-export {MAX_GAMES_TO_PLAY, BOARD_SIZE, cols, rows, MOVE_ANIMATION_DURATION_MS}
--- checkers-ui/pieces.js
+++
@@ -1,9 +0,0 @@
-function crownTheQueen(piece) {
-    // adds class piece--queen for piece parameter and div child with class of piece--queen-decoration
-    piece.classList.add("piece--queen");
-    const queenDecoration = document.createElement("div");
-    queenDecoration.classList.add("piece--queen-decoration");
-    piece.appendChild(queenDecoration);
-}
-
-export {crownTheQueen}
